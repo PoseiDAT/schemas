64290ed594e01c9798f2c0adee013242f5b05314
Mark Doeswijk <mark@springtree.nl>
2020-12-02 17:12:23 +0100

    feat(entry): add new type classes

---
+++ src/entry-zone-exit.ts
@@ -0,0 +1,11 @@
+import { BaseEntry } from './base-entry';
+import { schemas } from './schema';
+import { IEntryZoneExit } from './schema/types';
+
+export class ZoneExitEntry extends BaseEntry {
+  constructor(
+    public data: IEntryZoneExit,
+  ) {
+    super(data, schemas.entry.arrival);
+  }
+}
