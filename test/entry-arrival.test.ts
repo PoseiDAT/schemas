c5b666ab2d8510d3d835228e47dac05a7606ee42
Pieter Post <pieter@springtree.nl>
2021-02-04 17:21:13 +0100

    feat(test): added arrival entry unit test

--- test/entry-arrival.test.ts
+++ test/entry-arrival.test.ts
@@ -1,5 +1,6 @@
 import { IEntryArrival } from '../src/schema/types';
 import { ArrivalEntry } from '../src/index';
+import { v4 } from 'uuid';
 
 describe('ArrivalEntry', () => {
 
@@ -18,5 +19,34 @@ describe('ArrivalEntry', () => {
     const validationErrors = baseEntry.validate();
     expect(validationErrors).toBeDefined();
     expect(validationErrors.length).toBeGreaterThan(0);
+  });
+
+  test('Validating an minimal Arrival Entry should succeed', () => {
+    const data: IEntryArrival =
+      {
+        entry_id: v4(),
+        journal_id: v4(),
+        entry_type: "arrival",
+        entry_datetime: "2021-02-02T09:58:32z",
+        remarks: "This is a test",
+        trip : {
+            date: "2021-02-02T09:58:32z",
+            trip_nr: "NLD",
+            record_nr: "NLD"
+        },
+        activity_date: "2021-02-02T09:58:32z",
+        port: {
+            code: "NLURK"
+        },
+        revision: "2021-02-02T09:58:32z",
+        immutable: false,
+        reason_arrival: 'ECY',
+    };
+
+    const entry = new ArrivalEntry(data);
+
+    const validationErrors = entry.validate();
+    expect(validationErrors).toBeDefined();
+    expect(validationErrors.length).toEqual(0);
   });
 });
